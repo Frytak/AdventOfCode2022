2fc06f73010106f1a9ad1e86006fdab0072cf7f2
Frytak <71070897+Frytak@users.noreply.github.com>
2022-12-02 22:23:27 +0100

    Delete day_2 directory

--- day_2/target/debug/deps/day_2-1095277fb2ad57d8.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Frytak\Desktop\~\Important\Programming Projects\AdventOfCode2022\Day_2\target\debug\deps\day_2-1095277fb2ad57d8.rmeta: src\main.rs
-
-c:\Users\Frytak\Desktop\~\Important\Programming Projects\AdventOfCode2022\Day_2\target\debug\deps\day_2-1095277fb2ad57d8.d: src\main.rs
-
-src\main.rs:
--- day_2/target/debug/deps/day_2.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Frytak\Desktop\~\Important\Programming Projects\AdventOfCode2022\Day_2\target\debug\deps\day_2.exe: src\main.rs
-
-C:\Users\Frytak\Desktop\~\Important\Programming Projects\AdventOfCode2022\Day_2\target\debug\deps\day_2.d: src\main.rs
-
-src\main.rs:
